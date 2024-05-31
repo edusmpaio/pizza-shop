35126e8fe35884d3a52b8d587d6aa824a72d6fa7
Eduardo Sampaio <edupcsampaio@gmail.com>
2024-05-31 12:27:17 -0300

    feat: header default ui

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -29,7 +29,6 @@ export function SignUp() {
   async function handleSignUp(data: SignUpForm) {
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000))
-      console.log(data)
 
       toast.success('Restaurante cadastrado com sucesso.', {
         action: {
